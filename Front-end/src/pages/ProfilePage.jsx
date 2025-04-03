d5ee803dc6a3d61bee4447b75fd8f57d6f6a45d7
Sittipu <stpppr2548@gmail.com>
2025-04-03 23:35:12 +0700

    Template
    fixing

--- Front-end/src/pages/ProfilePage.jsx
+++ Front-end/src/pages/ProfilePage.jsx
@@ -14,24 +14,15 @@ const ProfilePage = () => {
   };
 
   return (
-<<<<<<< Updated upstream
-    <>
+      <div className="flex flex-col-reverse lg:flex-col "> 
       <Navbar />
+      <div className="Background w-screen h-screen flex flex-col items-start p-2 lg:p-10"></div>
       <div className="Background w-screen h-screen flex flex-col items-start p-10">
         
-        <h1 className="text-[60px] font-medium font-[Rajdhani]">Hello! Manow</h1>
-        <p className="text-gray-600 text-[30px] font-medium font-[Rajdhani]">KiminoNaWa@gmail.com</p>
-        <div className="flex flex-col w-full justify-center items-center">
-        <div className="flex items-center justify-center gap-4 my-4">
-=======
-      <div className='Background'>
-      <div className="flex flex-col-reverse lg:flex-col h-screen"> 
-      <Navbar />
         <h1 className="text-[54px] lg:text-[60px] font-medium, font-[Rajdhani]">Hello! Manow</h1>
         <p className="text-black text-[24px] lg:text-gray-600 lg:text-[30px] font-[Rajdhani]">KiminoNaWa@gmail.com</p>
         <div className="flex flex-col w-full justify-center items-center">
-        <div className="SecondaryBackground flex items-center rounded-[10px] drop-shadow-2xl justify-center gap-4 my-4 shadow lg:bg-[#ececec] lg:shadow-none">
->>>>>>> Stashed changes
+        <div className="flex items-center justify-center gap-4 my-4 bg-[#f2f2f2] shadow lg:bg-[#ececec] lg:shadow-none">
           
           <button className="p-3 cursor-pointer">
             <img src={left} className = "w-10" /> 
@@ -57,11 +48,7 @@ const ProfilePage = () => {
           Sign out
         </button>
       </div>
-<<<<<<< Updated upstream
-    </>
-=======
       </div>
->>>>>>> Stashed changes
   );
 };
 
