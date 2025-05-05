be5fc6e2d3f83d1bb22c05e3c9a3d5cdc86c56d4
Sittipu <stpppr2548@gmail.com>
2025-05-05 22:46:58 +0700

    backend updating

---
+++ Back-end/src/models/user.model.ts
@@ -0,0 +1,64 @@
+import { db } from "../index.ts";
+
+const isDuplicate = async( username: string, email: string ) => {
+    const user = await db.user.findFirst({
+        where: {
+            username: username,
+            email: email,
+        },
+    });
+    return user;
+}
+
+const signinUser = async ( name: string, password: string) => {
+    const user = await db.user.findFirst({
+        where: {
+            OR : [ {username : name },{ email : name } ],
+            password: password
+            }
+    })
+    return user;   
+}
+
+const createUser = async( username: string, email: string, password: string ) => {
+    const user = await db.user.create({
+        data: {
+            username: username,
+            email: email,
+            password: password,
+        },
+    });
+    return user;
+}
+
+const getAllUser = async () => {
+    const user = await db.user.findMany();
+    return user;
+}
+
+const getDiaryFromUser = async (month: string, userId: number) => {
+    const diary = await db.diary.findMany({
+        where: {
+            month: month,
+            userId: userId
+        }
+    })
+    return diary;
+}
+
+const changeTemplate = async (id: number, template: string) => {
+    const user = await db.template.update({
+        where: {
+            id: id
+        },
+        data: {    
+            bgColor   :"#000000",
+            bg2Color  :"#000000",
+            navColor  :"#000000",
+        },
+    })
+    return user;
+}
+
+
+export { isDuplicate, createUser, getAllUser, signinUser, getDiaryFromUser, changeTemplate }
